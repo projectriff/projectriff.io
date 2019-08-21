ba94300c71d7ce35f05c104777e3c33e4a371853
Scott Andrews <sandrews@pivotal.io>
2019-08-20 22:50:45 -0400

    Add copy to clipboard button to code blocks (#118)

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -62,6 +62,13 @@ const siteConfig = {
   // show all blog posts in sidebar
   blogSidebarCount: '5',
 
+  // add code copy-to-clipboard
+  scripts: [
+    'https://buttons.github.io/buttons.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js',
+    '/js/code-block-buttons.js',
+  ],
+  stylesheets: ['/css/code-block-buttons.css']
 };
 
 module.exports = siteConfig;
